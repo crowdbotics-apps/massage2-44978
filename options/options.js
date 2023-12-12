73961274688e55ad5a3475f308bb8e44caa1e8f9
Crowdbotics <team@crowdbotics.com>
2023-12-12 20:14:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "massage2-44978",
+  url: "https://massage2-44978.botics.co",
+  api: "https://massage2-44978.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
